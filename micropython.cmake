7433f984c33f1370c36bd4ed12553d83b5025026
Liu Zhongwei <liuzhongwei@espressif.com>
2024-12-11 18:48:49 +0800

    fix(repo): compile *.cpp files on MicroPython

--- micropython.cmake
+++ micropython.cmake
@@ -4,10 +4,11 @@ add_library(usermod_esp_io_expander INTERFACE)
 
 # Set the source directorya and find all source files.
 set(SRC_DIR ${CMAKE_CURRENT_LIST_DIR}/src)
-file(GLOB_RECURSE SRCS ${SRC_DIR}/*.c)
+file(GLOB_RECURSE SRCS_C ${SRC_DIR}/*.c)
+file(GLOB_RECURSE SRCS_CXX ${SRC_DIR}/*.cpp)
 
 # Add our source files to the library.
-target_sources(usermod_esp_io_expander INTERFACE ${SRCS})
+target_sources(usermod_esp_io_expander INTERFACE ${SRCS_C} ${SRCS_CXX})
 
 # Add the current directory as an include directory.
 target_include_directories(usermod_esp_io_expander INTERFACE ${SRC_DIR})
